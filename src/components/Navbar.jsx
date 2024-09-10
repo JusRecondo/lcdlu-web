3faaee4daf40db49e72d6f6817690125602f65f3
JusRecondo <justina.recondo@gmail.com>
2024-09-10 16:28:09 -0300

    Add icons in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,40 @@
 import { Link } from 'react-router-dom';
+import { FiInfo, FiMusic, FiFilm, FiLink } from 'react-icons/fi';
 
 const Navbar = () => {
     return (
         <ul className="navbar">
             <li>
-                <Link to="/bio">B</Link>
+                <Link
+                    to="/bio"
+                    aria-label="Bio"
+                >
+                    <FiInfo />
+                </Link>
             </li>
             <li>
-                <Link to="/musica">M</Link>
+                <Link
+                    to="/musica"
+                    aria-label="Música"
+                >
+                    <FiMusic />
+                </Link>
             </li>
             <li>
-                <Link to="/videos">V</Link>
+                <Link
+                    to="/videos"
+                    aria-label="Videos"
+                >
+                    <FiFilm />
+                </Link>
             </li>
             <li>
-                <Link to="/links">L</Link>
+                <Link
+                    to="/links"
+                    aria-label="Links"
+                >
+                    <FiLink />
+                </Link>
             </li>
         </ul>
     );
