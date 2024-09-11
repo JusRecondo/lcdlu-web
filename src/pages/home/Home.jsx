7772d8f3c1dd19bf507b78950eb7d956ad3f947a
JusRecondo <justina.recondo@gmail.com>
2024-09-11 10:34:01 -0300

    Apply changes in home page fot desktop size and add announcement

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,36 @@
-import homebg from './homebg.jpg';
+/* import BandName from './BandName'; */
+import lacaradelosultimos from './lacaradelosultimos.jpg';
+import lacaradelosultimosnombre from './lacaradelosultimosnombre.png';
+import { FiArrowRight } from 'react-icons/fi';
+
 const Home = () => {
     return (
         <section id="home">
             <img
-                src={homebg}
+                src={lacaradelosultimosnombre}
+                role="presentation"
+                loading="lazy"
+                className="homebgnombre"
+            />
+            <img
+                src={lacaradelosultimos}
                 role="presentation"
                 loading="lazy"
                 className="homebg"
             />
+            <article className="announcement">
+                <h2>Próxima fecha</h2>
+                <p>21 de Septiembre en Haedo</p>
+                <p className="more-info">
+                    Mas info en <FiArrowRight />
+                    <a
+                        href="https://www.instagram.com/after.indie/"
+                        target="_blank"
+                    >
+                        @after.indie
+                    </a>{' '}
+                </p>
+            </article>
         </section>
     );
 };
