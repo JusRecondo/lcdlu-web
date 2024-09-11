1f42a6fb748c582ac3bd9b9e33b56ba279d0b87b
JusRecondo <justina.recondo@gmail.com>
2024-09-10 22:53:14 -0300

    Add links page with json data

--- src/pages/Links.jsx
+++ src/pages/Links.jsx
@@ -1,10 +1,38 @@
 import CommonContentSection from '../components/common/CommonContentSection';
+import data from '../assets/data.json';
 
 const Links = () => {
     return (
-        <CommonContentSection title="Links">
-            <p>lksadlaksdjl</p>
-            <p>asdasdsad</p>
+        <CommonContentSection
+            title="Links"
+            id="links"
+        >
+            <h3>Redes y Plataformas</h3>
+            <ul>
+                {data.links?.socialAndPlatforms.map((item, i) => (
+                    <li key={i}>
+                        <a
+                            href={item.link}
+                            target="_blank"
+                        >
+                            {item.text}
+                        </a>
+                    </li>
+                ))}
+            </ul>
+            <h3>Prensa</h3>
+            <ul>
+                {data.links?.press.map((item, i) => (
+                    <li key={i}>
+                        <a
+                            href={item.link}
+                            target="_blank"
+                        >
+                            {item.text}
+                        </a>
+                    </li>
+                ))}
+            </ul>
         </CommonContentSection>
     );
 };
