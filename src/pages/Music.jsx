470f657e5f7e802dd415397a4d74a63dd8b87296
JusRecondo <justina.recondo@gmail.com>
2024-09-10 22:28:26 -0300

    Apply responsive styles in music page

--- src/pages/Music.jsx
+++ src/pages/Music.jsx
@@ -20,27 +20,33 @@ const Music = () => {
             <Slider {...sliderSettings}>
                 {data.music?.map((item, i) => (
                     <div key={i}>
-                        <h3>
-                            {item.name} - {item.year} ({item.type})
-                        </h3>
-                        <iframe
-                            src={item.embed}
-                            width="100%"
-                            height="352"
-                            frameBorder="0"
-                            allowFullScreen=""
-                            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-                            loading="lazy"
-                        ></iframe>
-                        <p>{item.summary}</p>
-                        {item.video && (
-                            <a
-                                href={item.video.link}
-                                target="_blank"
-                            >
-                                {item.video.text}
-                            </a>
-                        )}
+                        <div className="slider-item-inner-container">
+                            <div>
+                                <h3>
+                                    {item.name} - {item.year} ({item.type})
+                                </h3>
+                                <iframe
+                                    src={item.embed}
+                                    width="100%"
+                                    height="352"
+                                    frameBorder="0"
+                                    allowFullScreen=""
+                                    allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                                    loading="lazy"
+                                ></iframe>
+                            </div>
+                            <div>
+                                <p>{item.summary}</p>
+                                {item.video && (
+                                    <a
+                                        href={item.video.link}
+                                        target="_blank"
+                                    >
+                                        {item.video.text}
+                                    </a>
+                                )}
+                            </div>
+                        </div>
                     </div>
                 ))}
             </Slider>
