d65fc74c8f1f5ee172922f12045cb7effa82b037
JusRecondo <justina.recondo@gmail.com>
2024-09-16 10:04:27 -0300

    Implement content in videos page

--- src/pages/videos/Videos.jsx
+++ src/pages/videos/Videos.jsx
@@ -1,10 +1,50 @@
 import CommonContentSection from '../../components/common/CommonContentSection';
+import data from '../../assets/data.json';
+import Slider from 'react-slick';
 
 const Videos = () => {
+    const sliderSettings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        adaptiveHeight: true,
+    };
     return (
-        <CommonContentSection title="Videos">
-            <p>lksadlaksdjl</p>
-            <p>asdasdsad</p>
+        <CommonContentSection
+            title="Videos"
+            id="videos"
+        >
+            <Slider {...sliderSettings}>
+                {data.videos?.map((item, i) => (
+                    <div key={i}>
+                        <div className="slider-item-inner-container">
+                            <div>
+                                <h3>
+                                    {item.name} - {item.year} ({item.type})
+                                </h3>
+                                <div className="video-wrapper">
+                                    <iframe
+                                        width="560"
+                                        height="315"
+                                        src={item.embed}
+                                        title="YouTube video player"
+                                        frameBorder="0"
+                                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                        referrerPolicy="strict-origin-when-cross-origin"
+                                        allowFullScreen
+                                    ></iframe>
+                                </div>
+                            </div>
+                            <div>
+                                <p>{item.summary}</p>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </Slider>
         </CommonContentSection>
     );
 };
